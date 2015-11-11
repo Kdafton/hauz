2fbb33e4b95d2baf1162c53916f26e28f5ba02d9
dafton <dafton>
2015-11-11 16:17:30 +0300

    setting up views

---
+++ main.php
@@ -0,0 +1,70 @@
+<html>
+	<head>
+		<title>
+			hauz
+		</title>
+		   <meta charset="utf-8">
+		   <meta name="viewport" content="width=device-width, initial-scale=1">
+		   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		   <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		   <script src="bootstrap4/docs/assets/js/vendor/jquery.min.js"></script>
+		   <script src="bootstrap/js/bootstrap.min.js"></script>
+		   <script src="star/js/star-rating.min.js"></script>
+		   <link rel="stylesheet" href="star/css/star-rating.min.css">
+		   <style type="text/css">
+		   </style>
+	</head>
+	<body background="images/back6.jpg">
+		<div class="jumbotron">
+		
+		</div>
+		<nav class="navbar navbar-inverse navbar-top" style="height:60px;">
+	  		<div class="container-fluid">
+	    		<div class="navbar-header">
+	      			<a class="navbar-brand" href="#">bla</a>
+	    		</div>
+	    	<div>
+		      <ul class="nav navbar-nav">
+		        <li><a href=".php">bed seaters</a></li>
+				<li><a href="">single rooms</a></li>
+				<li><a href="">one bedroom</a></li>
+				<li><a href=""> two bedroom</a></li>
+				<li><a href="">three bedroom</a></li>
+		      </ul>
+	      	  <ul class="nav navbar-nav navbar-right">
+		        <li><a href="register.html"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+		        <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	      	  </ul>
+		    </div>
+		  </div>
+		</nav>
+		
+		
+		
+		<div class="container">
+			<div class="row">
+			<br><br><br><br><br><br>
+				<form class="form-horizontal" role="form">
+				  <div class="form-group">
+				    <div class="col-sm-7 col-sm-offset-2">
+				      <input type="search" class="form-control" id="search" placeholder="search by price or house types" style="height:60px;">
+				      </div>
+				      <div class="col-sm-3.5 col-sm-offset-7.5">
+					  <button type="button" class="btn btn-info" style="height:60px; width:150px">
+					    <span class="glyphicon glyphicon-search"></span> Search
+					  </button>
+					  </div>
+				  </div>
+				</form>
+
+				<div class="col-sm-3">
+					
+				</div>
+
+			</div>
+		</div>
+		
+
+
+	</body>
+</html>
